118c96064f4574c0442a7208229a8dc82209cf88
zhangliang <zhangliang>
2017-09-11 17:31:18 +0800

    nothing

--- common/client_msg/message.go
+++ common/client_msg/message.go
@@ -7,14 +7,6 @@ type Proto struct {
 	Body string `json:"body"`
 }
 
-type MsgMove struct {
-	Angle float32 `json:"angle"`
-	Speed float32 `json:"speed"`
-}
-type MsgJoinRoom struct {
-	RoomId int64 `json:"room_id"`
-}
-
 func GetProto(data []byte) *Proto {
 	p := Proto{}
 	json.Unmarshal(data, &p)
@@ -27,21 +19,7 @@ func GetBody(data []byte) []byte {
 	return []byte(p.Body)
 }
 
-// {cmd:0,body:'{angle,speed}'}
-func GetMove(data []byte) *MsgMove {
-	m := MsgMove{}
-	json.Unmarshal(GetBody(data), &m)
-	return &m
-}
-
-// {cmd:0,body:'{room_id}'}
-func GetJoinRoom(data []byte) *MsgJoinRoom {
-	m := MsgJoinRoom{}
-	json.Unmarshal(GetBody(data), &m)
-	return &m
-}
-
-func NewProto(cmd int,body []byte)[]byte{
-	bs,_:=json.Marshal(&Proto{Cmd:cmd,Body:string(body)})
+func NewProto(cmd int, body []byte) []byte {
+	bs, _ := json.Marshal(&Proto{Cmd: cmd, Body: string(body)})
 	return bs
 }
--- common/cmd.go
+++ common/cmd.go
@@ -1,12 +1,10 @@
 package common
 
-
 const (
-	CMD_JoinRoom = 100
-	CMD_Broad    = 110 // 广播给所有人
-	CMD_Login    = 1
-	
-	CMD_InitPlayer = 1000
-	CMD_Logic = 1100
-)
+	CMD_JoinRoom = 100 // 加入房间
+	CMD_Broad    = 110 // 广播给同一actor的所有人
+	CMD_Login    = 1   // 登录请求
 
+	CMD_InitPlayer = 1000 // 
+	CMD_Logic      = 1100
+)
